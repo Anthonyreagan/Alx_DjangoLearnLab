c9ccdad771f1f807ecb658aee0f4be4f4bcb1592
Anthony <anthonyreagan126@gmail.com>
2025-08-02 13:52:08 +0300

    adding custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,23 @@
+
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser, BookLoan
+from django.contrib.auth.admin import UserAdmin
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo', 'role', 'membership_date')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo', 'role')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(BookLoan)
