5881fd8c2e32be081d87981ab24cb06c1ff2dadf
Anthony <anthonyreagan126@gmail.com>
2025-08-03 23:01:16 +0300

    API Endpoint with Django REST Framework mandatory

---
+++ api_project/api/serializers.py
@@ -0,0 +1,11 @@
+# api_project/api/serializers.py
+
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'  # This will include all fields from the Book model
+        # Alternatively, you can specify fields explicitly:
+        # fields = ['id', 'title', 'author']
